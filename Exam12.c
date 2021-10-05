8049ae91b47e3887b26a5b901892d3db1045efe3
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-05 14:30:05 +0300

    Create Exam12.c

---
+++ Exam12.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("# # # # # # # # # # #\n");
+    printf("  # # # # # # # # # \n");
+    printf("    # # # # # # #\n");
+    printf("      # # # # #\n");
+    printf("        # # #\n");
+    printf("          #\n");
+    printf("         (a)    \n\n\n");
+
+printf("\n");
+printf("\n");
+printf("\n");
+
+    printf("            #\n");
+    printf("          # # #\n");
+    printf("        # # # # #\n");
+    printf("      # # # # # # #\n");
+    printf("    # # # # # # # # #\n");
+    printf("  # # # # # # # # # # #\n");
+    printf("           (b)");
+
+printf("\n");
+printf("\n");
+printf("\n");
+
+
+    printf("            #\n");
+    printf("          # # #\n");
+    printf("        # # # # #\n");
+    printf("      # # # # # # #\n");
+    printf("    # # # # # # # # #\n");
+    printf("  # # # # # # # # # # #\n");
+	printf("    # # # # # # # # #\n");
+	printf("      # # # # # # #\n");
+	printf("        # # # # #\n");
+	printf("          # # #\n");
+	printf("            #\n");
+    printf("           (c)");
+    return 0;
+
+}
