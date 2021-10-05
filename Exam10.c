0fce79cfc0f9d74597122a6260da8bebc0d3cdd0
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-05 14:29:19 +0300

    Create Exam10.c

---
+++ Exam10.c
@@ -0,0 +1,52 @@
+/*
+10. Представете си, че имате фирма за отдаване на каравани и кемпери под
+наем, за която трябва да разработите софтуер. Вие сте малка фирма и имате
+общо 3 каравани на цена 90 лв. на ден и 3 кемпера на цена 100 лв. на ден.
+Декларирайте променливи за броя на караваните, цената на караваните, броя на
+кемперите и цената им. Направете меню, което пита клиента каравана или
+кемпер ще иска. Направете променлива, в която да се събират парите, които
+клиента дължи на компанията. Принтирайте резултата
+*/
+#include <stdio.h>
+
+int main() {
+    int iTotalCaravan = 3;
+    float fTotalCaravanPrice = 90.0;
+    float fCaravanPrice = fTotalCaravanPrice / iTotalCaravan;
+
+    int iTotalCamper = 3;
+    float fTotalCamperPrice = 100.0;
+    float fCamperPrice = fTotalCamperPrice / iTotalCamper;
+
+    float fDays = 0.0;
+
+    float totalAmount = 0.0;
+
+    int iChoice = 0;
+
+    printf(" -- Enter 1 for rent caravan\n");
+    printf(" -- Enter 2 for rent camper\n");
+
+    printf(" -- Enter your choice: ");
+
+    scanf("%d", &iChoice);
+
+    if(iChoice == 1) {
+        printf("How many days would you like for your vacation: ");
+        scanf("%f", &fDays);
+        totalAmount = fDays * fCaravanPrice;
+        printf("You rent a caravan for %.0f days, price for day is %.2fBGN\n", fDays, fCaravanPrice);
+        printf("Total amount to pay is: %.2fBGN", totalAmount);
+    } else if(iChoice == 2) {
+         printf("How many days would you like for your vacation: ");
+         scanf("%f", &fDays);
+         totalAmount = fDays * fCamperPrice;
+         printf("You rent a caravan for %.0f days, price for day is %.2fBGN\n", fDays, fCamperPrice);
+         printf("Total amount to pay is: %.2fBGN", totalAmount);
+    }
+    else {
+        printf("Invalid choice\n");
+    }
+
+    return 0;
+}
