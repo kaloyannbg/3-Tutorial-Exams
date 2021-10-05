cb3d694b021a8c883f2ad292dc582be81ed1178e
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-05 17:36:33 +0300

    Update Exam10.c

--- Exam10.c
+++ Exam10.c
@@ -12,15 +12,19 @@
 int main() {
     int iTotalCaravan = 3;
     float fCaravanPrice = 90.0;
+    int iAvailableCaravan = 3;
 
     int iTotalCamper = 3;
     float fCamperPrice = 100.0;
+    int iAvailableCamper = 3;
 
     float fDays = 0.0;
 
     float totalAmount = 0.0;
 
     int iChoice = 0;
+
+    int rentNum = 0;
 
     printf(" -- Enter 1 for rent caravan\n");
     printf(" -- Enter 2 for rent camper\n");
@@ -30,17 +34,30 @@ int main() {
     scanf("%d", &iChoice);
 
     if(iChoice == 1) {
-        printf("How many days would you like for your vacation: ");
-        scanf("%f", &fDays);
-        totalAmount = fDays * fCaravanPrice;
-        printf("You rent a caravan for %.0f days, price for day is %.2fBGN\n", fDays, fCaravanPrice);
-        printf("Total amount to pay is: %.2fBGN", totalAmount);
+        printf("How many caravans would you like to rent: ");
+        scanf("%d", &rentNum);
+        if(iAvailableCaravan < rentNum) {
+            printf("Sorry but we dont have %d avaialable", rentNum);
+        }else {
+            iAvailableCamper = iAvailableCamper - rentNum;
+            printf("How many days would you like for your vacation: ");
+            scanf("%f", &fDays);
+            totalAmount = fDays * (fCaravanPrice * rentNum);
+            printf("You rent %d caravan for %.0f days, price for day per caravan is %.2fBGN\n", rentNum, fDays, fCaravanPrice);
+            printf("Total amount to pay is: %.2fBGN", totalAmount);
+        }
     } else if(iChoice == 2) {
-         printf("How many days would you like for your vacation: ");
-         scanf("%f", &fDays);
-         totalAmount = fDays * fCamperPrice;
-         printf("You rent a caravan for %.0f days, price for day is %.2fBGN\n", fDays, fCamperPrice);
-         printf("Total amount to pay is: %.2fBGN", totalAmount);
+        printf("How many caravans would you like to rent: ");
+        scanf("%d", &rentNum);
+        if(iAvailableCamper < rentNum) {
+            printf("Sorry but we dont have %d avaialable", rentNum);
+        }else {
+            printf("How many days would you like for your vacation: ");
+            scanf("%f", &fDays);
+            totalAmount = fDays * (fCamperPrice * rentNum);
+            printf("You rent %d caravan for %.0f days, price per day is %.2fBGN\n", rentNum, fDays, fCamperPrice);
+            printf("Total amount to pay is: %.2fBGN", totalAmount);
+        }
     }
     else {
         printf("Invalid choice\n");
