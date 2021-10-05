9ca43819e7d6a5bb7242afb5fdec764d55f81bcc
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-05 15:45:48 +0300

    Update Exam9.c

--- Exam9.c
+++ Exam9.c
@@ -3,12 +3,12 @@
 int main() {
 
     double dDouble = 4.9876543;
-    long double lDouble = 7.123456789012345678890;
+    double lDouble = 7.123456789012345678890; //cut our symbols after digit: 5
      long double lDoubleTwo =  18398458438583853.28;
      long double dllDouble = 18398458438583853.39875937284928422;
 
     printf("%.7f\n", dDouble);
-    printf("%.20Lf\n", lDouble);
+    printf("%.20f\n", lDouble);
     printf("%lE\n", lDoubleTwo);
     printf("%llf\n", dllDouble);
 
